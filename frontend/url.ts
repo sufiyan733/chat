b9a528de4ca52e629c155207c8208390cc0e54ec
sufiyan733 <kanekigaming786@gmail.com>
2025-11-13 04:09:28 +0530

    hgsgd

--- frontend/url.ts
+++ frontend/url.ts
@@ -1,2 +1,2 @@
-export const chatt_service="https://sufiyanbe.duckdns.org/svc5002"
-export const userr_service="https://sufiyanbe.duckdns.org/svc3001"
+export const chatt_service="https://sufiyanbe.duckdns.org/chat"
+export const userr_service="https://sufiyanbe.duckdns.org/user"
